a8ce6e3de5696084a92e84a7f5fe00885c7cbb89
shiruiuiui <shirui.us@gmail.com>
2018-07-17 21:12:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+end
+
+  get '/name' do
+    "My name is Shirui Zhong"
+end
+
+  get '/hometown' do 
+    "My hometown is Morgantown."
+end
+
+  get '/favorite-song' do
+    "My favorite song is no"
+end
 end
